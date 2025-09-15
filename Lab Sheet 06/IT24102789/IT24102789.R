430eae7b72c065aaa44d7819a5850db41321f8a5
IT24102789 <IT24102789@my.sliit.lk>
2025-09-15 10:32:39 +0530

    Lab6_PS

---
+++ Lab Sheet 06/IT24102789/IT24102789.R
@@ -0,0 +1,16 @@
+#Exersize - lab6- IT24102789
+
+#1
+#i - Binomial Distribution
+  
+#ii - 
+1 - pbinom(46,50,0.85, lower.tail = TRUE)
+
+#2
+
+#i. - Number of  customers call per hour
+
+#ii - Possion Distribution
+
+#iii
+dpois(15,12)
