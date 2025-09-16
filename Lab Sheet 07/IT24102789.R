06fc6ec1d3baf06df8d3967fcb8cea0c940e7a7d
IT24102789 <IT24102789@my.sliit.lk>
2025-09-16 11:43:23 +0530

    PS_Lab7_R

---
+++ Lab Sheet 07/IT24102789.R
@@ -0,0 +1,16 @@
+# Lab7_PS - IT24102789 - Exercise
+
+setwd("C:\\Users\\Anusara\\OneDrive\\Desktop\\IT24102789")
+
+#1
+punif(25,min=0,max=40,lower.tail = TRUE) - punif(10,min=0,max=40,lower.tail = TRUE)
+
+#2
+pexp(2,rate = 0.33,lower.tail = TRUE)
+
+#3
+#i
+pnorm(130, mean = 100, sd= 15 , lower.tail=FALSE)
+
+#ii
+qnorm(0.95,mean = 100, sd= 15 , lower.tail=TRUE)
