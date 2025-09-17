867dd78b13df1359fe3dd95480875125954c3a7a
IT24102789 <IT24102789@my.sliit.lk>
2025-09-17 11:04:01 +0530

    Lab6_Rfile

---
+++ Lab Sheet 06/IT24102789.R
@@ -0,0 +1,17 @@
+#Exersize - lab6- IT24102789
+setwd("C:\\Users\\Anusara\\Downloads\\Y2S1Edu\\PS\\Labs\\Lab 06-20250915\\IT24102789")
+#1
+#i - Binomial Distribution
+  
+#ii - 
+1 - pbinom(46,50,0.85, lower.tail = TRUE)
+
+#2
+
+#i. - Number of  customers call per hour
+
+#ii - Possion Distribution
+
+#iii
+dpois(15,12)
+
